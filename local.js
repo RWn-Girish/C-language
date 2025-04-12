f9eb816175fd6878d1b9019b9ae83f55160e5174
Girish Gondaliya <rw3.girish.gk@gmail.com>
2025-04-12 16:39:23 +0530

    git hub

---
+++ local.js
@@ -0,0 +1,16 @@
+// function mul(a, b){
+//    return a * b
+// }
+
+// module.exports = mul;   // export defualt
+
+
+// named export
+
+exports.add = (a,b) => {
+    return a + b;
+}
+
+exports.sub = (a,b) => {
+    return a - b
+}
---
+++ modlue.js
@@ -0,0 +1,26 @@
+// Modules => Functionality 
+
+// Core Module => in-built Module 
+//              http - server, 
+//              fs - file handle, 
+//              OS - Operating Sys, 
+//              Path - path related work
+//              URL - URL related
+
+// Local Module => User Defined
+
+// Third party => npm (node package manager)
+
+
+// const local = require('./local');
+
+// const {add, sub} = require('./local')
+// console.log(local(10, 20))
+
+// console.log(add(25,26));
+// console.log(sub(35,20))
+
+const os = require('os');
+// console.log(os.networkInterfaces())
+console.log(os.homedir())
+console.log(os.hostname())
